9bcda73b765589195a97ca310dfaa8ae4599ec36
Flzomb <spark3840@gmail.com>
2024-09-26 11:38:30 +0900

    remove-kafka

--- PyHwpServer/starter.bat
+++ PyHwpServer/starter.bat
@@ -1,4 +1,4 @@
 @echo off
-start kafka\bin\windows\zookeeper-server-start.bat kafka\config\zookeeper.properties
+start bin\windows\zookeeper-server-start.bat config\zookeeper.properties
 timeout /t 10 > nul
-start kafka\bin\windows\kafka-server-start.bat kafka\config\server.properties
+start bin\windows\kafka-server-start.bat config\server.properties
